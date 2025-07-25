25d46801d1bd4063415b7a623e368c7dd0332e56
eunbin55 <eunbin620@naver.com>
2025-07-25 20:53:11 +0900

    deploy

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
+require("dotenv").config();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", indexRouter);
 
-const mongoURI = `mongodb://localhost:27017/todo-demo`;
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(() => console.log("mongoose connected"))
   .catch((err) => console.log("DB connection fail", err));
 
-app.listen(5000, () => console.log("server is on 5000"));
+app.listen(process.env.PORT || 5000, () => console.log("server is on 5000"));
