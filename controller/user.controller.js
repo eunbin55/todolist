52da24f2f2b703ca3866068a164da80a2f3f783d
eunbin55 <eunbin620@naver.com>
2025-07-27 20:16:21 +0900

    createUser and login

---
+++ controller/user.controller.js
@@ -0,0 +1,48 @@
+const userController = {};
+const bcrypt = require("bcryptjs");
+const User = require("../model/User");
+const saltRounds = 10;
+
+// 회원가입
+userController.createUser = async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
+
+    // 기존 유저인지 확인
+    const user = await User.findOne({ email }); // findOne({email: email})와 같은 의미
+    if (user) {
+      throw new Error("이미 가입이 된 유저입니다.");
+    }
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+    const newUser = new User({ email, name, password: hash });
+    await newUser.save();
+    res.status(200).json({ status: "success" });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error });
+  }
+};
+
+// 로그인
+userController.loginWithEmail = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // "-createdAt" 은 이번 findOne 콜에서만 제거되는 일시적 제거 기능.
+    const user = await User.findOne({ email }, "-createdAt -updatedAt -__v");
+
+    if (user) {
+      const isMatch = bcrypt.compareSync(password, user.password);
+      if (isMatch) {
+        // 토큰 발행
+        const token = user.generateToken();
+        // 응답으로 유저 정보 및 토큰 보냄
+        return res.status(200).json({ status: "success", user, token });
+      }
+    }
+    throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
+  } catch (error) {
+    res.status(400).json({ status: "fail", error });
+  }
+};
+
+module.exports = userController;
---
+++ model/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// 프론트엔드에서 호출할 때 항상 실행되는 함수
+// 즉, password는 항상 제거하고 return
+userSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.password;
+
+  return obj;
+};
+
+// methods: 몽구스 모델에 함수를 추가할 때 사용하는 속성
+// this: generateToken을 호출한 객체
+userSchema.methods.generateToken = function () {
+  const token = jwt.sign({ _id: this.id }, JWT_SECRET_KEY, {
+    expiresIn: "1d", // 유효기간
+  });
+  return token;
+};
+const User = mongoose.model("User", userSchema);
+module.exports = User;
